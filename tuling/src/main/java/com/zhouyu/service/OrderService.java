726a59a20d72195e486219c47fe1457373e623c6
archguide <a497269678@qq.com>
2021-10-08 16:56:10 +0800

    AOP注释

---
+++ tuling/src/main/java/com/zhouyu/service/OrderService.java
@@ -0,0 +1,10 @@
+package com.zhouyu.service;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * @author 周瑜
+ */
+@Component
+public class OrderService {
+}
---
+++ tuling/src/main/java/com/zhouyu/service/UserService.java
@@ -0,0 +1,29 @@
+package com.zhouyu.service;
+
+import org.aopalliance.intercept.MethodInvocation;
+import org.springframework.aop.interceptor.ExposeInvocationInterceptor;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.interceptor.TransactionAspectSupport;
+import org.springframework.transaction.support.TransactionSynchronizationManager;
+
+import javax.annotation.PostConstruct;
+
+/**
+ * @author 周瑜
+ */
+@Component
+public class UserService {
+
+
+	public void test(){
+		System.out.println("test");
+	}
+
+
+
+}
