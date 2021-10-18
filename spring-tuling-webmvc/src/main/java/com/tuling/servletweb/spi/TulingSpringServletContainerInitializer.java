f38ba67b193ea1a1694030cd7740ed7e43600cf6
xulisha123 <258300881@qq.com>
2021-10-18 20:50:20 +0800

    徐庶springmvc

--- spring-tuling-webmvc/src/main/java/com/tuling/servletweb/spi/TulingSpringServletContainerInitializer.java
+++ spring-tuling-webmvc/src/main/java/com/tuling/servletweb/spi/TulingSpringServletContainerInitializer.java
@@ -17,7 +17,7 @@ import java.util.Set;
  * @Slogan 致敬大师，致敬未来的你
  */
 @HandlesTypes(WebApplicationInitializer.class)
-public abstract class TulingSpringServletContainerInitializer extends SpringServletContainerInitializer {
+public  class TulingSpringServletContainerInitializer extends SpringServletContainerInitializer {
 
     /**
      *
